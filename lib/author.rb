38bd083f00133781ce031dda62c80e8ffcd1f0ec
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-05 22:03:27 -0400

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,16 +1,18 @@
 class Author
+  #any instance of author will have the ability to read and write name and author
   attr_accessor :name, :author
-
+  #each instance will be initialized with name
   def initialize(name)
     @name = name
   end
-  
+  #posts method will return the array of all the posts from the Posts class
   def posts
     Post.all
   end
-
+  #add_post takes in an argument of post. we are setting the author equal to self,
+  #since each author instance is iniialized with a name
   def add_post(post)
-    post.author = self
+    post.author = self.name
   end
 
   def add_post_by_title(post)
--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,19 @@
 class Post
+  #any instance of post has the ability to write/read a title and author method
   attr_accessor :title, :author
-
+  #class variable set to an empty array that will eventually collect all the posts
   @@all = []
-
+  #upon initializing a new instance, must give it a title, and put the entire instance 
+  #ont the @@all class variable array
   def initialize(title)
     @title = title
     @@all << self
   end
-
+  #class method all returns the class variable all
   def self.all
     @@all
   end
-
+  #author name method so if author exists, author.name is the authors name
   def author_name
     author.name if author
   end
